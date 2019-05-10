bd9a1c1d13d649e7d1e441814f21b7a2ff0c70ed
xand <xandjiji@gmail.com>
2019-05-09 23:31:24 -0300

    code cleanup

--- main.js
+++ main.js
@@ -1,34 +1,23 @@
 var img = new Image();
 img.src = "image.jpg";
 
-var ctx = document.getElementById('imgInput').getContext('2d');
-var ctx2 = document.getElementById('imgOutput').getContext('2d');
-
 img.onload = function () {
 
-    ctx.canvas.width = img.width;
-    ctx.canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+    // height and width of loaded image
+    var w = img.width;
+    var h = img.height;
 
-    var data = ctx.getImageData(0, 0, img.width, img.height);
-    ctx2.canvas.width = img.width;
-    ctx2.canvas.height = img.height;
-    grayscale(data);
+    // drawing original image on canvas
+    var originalCanvas = document.getElementById('original').getContext('2d');
+    originalCanvas.canvas.width = w;
+    originalCanvas.canvas.height = h;
+    originalCanvas.drawImage(img, 0, 0);
 
-}
+    // drawing grayscale image on canvas
+    var grayscaleCanvas = document.getElementById('grayscale').getContext('2d');
+    grayscaleCanvas.canvas.width = w;
+    grayscaleCanvas.canvas.height = h;
+    var data = originalCanvas.getImageData(0, 0, w, h);
+    grayscaleCanvas.putImageData(grayscale(data), 0, 0);
 
-function grayscale(imgData) {
-    
-
-    for(var i = 0; i < imgData.data.length; i += 4){
-    
-        var r = imgData.data[i];
-        var g = imgData.data[i+1];
-        var b = imgData.data[i+2];
-    
-        var v = 0.2126*r + 0.7152*g + 0.0722*b;
-        imgData.data[i] = imgData.data[i+1] = imgData.data[i+2] = v
-    }    
-
-    ctx2.putImageData(imgData, 0, 0);
 }
